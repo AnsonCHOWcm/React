11ad085d148109166ce6f2798c598cec07d86c5d
AnsonCHOWcm <74531677+AnsonCHOWcm@users.noreply.github.com>
2023-01-27 03:23:30 +0800

    Finished Final Proj

--- react-learning-material/src/pages/FinalProjComponent/App.js
+++ react-learning-material/src/pages/FinalProjComponent/App.js
@@ -11,6 +11,14 @@ const App = () => {
         "confirmDicenumber" : false
     })
 
+    const [DiceStatus, setDiceStatus] = React.useState([])
+
+    const [GameMode, setGameMode] = React.useState(false)
+
+    const [GameWin, setGameWin] = React.useState(false)
+
+    const [ButtonContent, setButtonContent] = React.useState("Submit")
+
     const changeDiceNumber = (inputvalue) => {
         setnumberDice((prevnumberDice) => {
             return(
@@ -20,7 +28,26 @@ const App = () => {
                 }
             );
         })
-    }
+    };
+
+    React.useEffect(() => {
+
+        const alllocked = DiceStatus.every(item => item.locked)
+        let alleq
+        if (DiceStatus.length ==0) {
+            alleq = false
+        }else {
+            const firstnum = DiceStatus[0].number
+            alleq = DiceStatus.every(item => item.number == firstnum )
+        }
+
+        if (alllocked && alleq) {
+            setGameWin(true)
+        }
+
+
+
+    },[DiceStatus])
 
     const confirmDiceMethod = (isValidnumber) => {
         setnumberDice((prevnumberDice) => {
@@ -32,6 +59,82 @@ const App = () => {
                 }
             );
         })
+    };
+
+    const changeGameMode = () => {
+        setGameMode(prevGameMode => !prevGameMode)
+    };
+
+    const setupDiceStatus = (numberofdices) => {
+
+        let newDiceStatus = []
+
+        for (let i=0; i< numberofdices; i++){
+            let currentDice = {
+                id : i,
+                number : Math.floor(Math.random() * 6 + 1),
+                locked : false
+            }
+            newDiceStatus.push(currentDice)
+        }
+
+
+
+        setDiceStatus(newDiceStatus)
+    };
+
+    const drawDice = () => {
+
+        let newDiceStatus = []
+
+        for (let i=0; i< DiceStatus.length; i++){
+            let currentDice = 
+            !DiceStatus[i].locked ? {...DiceStatus[i], number : Math.floor(Math.random() * 6 + 1)} : DiceStatus[i]
+            newDiceStatus.push(currentDice)
+        }
+
+        setDiceStatus(newDiceStatus)
+    };
+
+    const toggleDiceLock = (event) => {
+
+        const id = event.target.id
+
+        let newDiceStatus = []
+
+        for (let i=0; i< DiceStatus.length; i++){
+            let currentDice = 
+            DiceStatus[i].id == id ? {...DiceStatus[i], locked : !DiceStatus[i].locked} : DiceStatus[i]
+            newDiceStatus.push(currentDice)
+        }
+
+        setDiceStatus(newDiceStatus)
+    };
+
+    const confirmGameWin = () => {
+        setGameWin(true)
+    }
+
+    const RestartnumberDice = () => {
+        setnumberDice({
+            "number" : "0",
+            "messageShown" : "Please enter the number of Dices",
+            "confirmDicenumber" : false
+        })
+    }
+
+    const RestartDiceStatus = () => {
+        setDiceStatus([])
+    }
+
+    const RestartGameMode = () => {
+        setGameMode(false)
+    }
+    const RestartGameWin = () => {
+        setGameWin(false)
+    }
+    const RestartButtonContent = () => {
+        setButtonContent("Submit")
     }
 
     const mode = numberDice.confirmDicenumber
@@ -43,8 +146,32 @@ const App = () => {
                 <br></br>
                 Click each dice to freeze it at its current value between rolls.
             </p>
-            {mode ? <Dice numberDice = {numberDice}/> : <GameEnteringQ numberDice = {numberDice} changeDiceMethod = {changeDiceNumber}/>}
-            <Button numberDice = {numberDice} confirmDiceMethod = {confirmDiceMethod}/>
+            {mode ? 
+            <Dice numberDice = {numberDice} 
+            DiceStatus = {DiceStatus}
+            toggleDiceLock = {toggleDiceLock}
+            /> : 
+            <GameEnteringQ numberDice = {numberDice} 
+            changeDiceMethod = {changeDiceNumber}
+            />}
+            <Button 
+            numberDice = {numberDice}
+            DiceStatus = {DiceStatus} 
+            GameWin = {GameWin}
+            GameMode = {GameMode} 
+            ButtonContent = {ButtonContent}
+            confirmDiceMethod = {confirmDiceMethod} 
+            setButtonContent = {setButtonContent}
+            changeGameMode = {changeGameMode} 
+            setupDiceStatus = {setupDiceStatus}
+            drawDice = {drawDice}
+            confirmGameWin = {confirmGameWin}
+            RestartnumberDice = {RestartnumberDice}
+            RestartDiceStatus = {RestartDiceStatus}
+            RestartGameMode = {RestartGameMode}
+            RestartGameWin = {RestartGameWin}
+            RestartButtonContent = {RestartButtonContent}
+            />
         </div>
     );
 };
--- react-learning-material/src/pages/FinalProjComponent/Dice.js
+++ react-learning-material/src/pages/FinalProjComponent/Dice.js
@@ -2,18 +2,26 @@ import React from "react";
 import "./FinalProj_style.css";
 
 const Dice = (props) => {
+
+    let DiceElements = props.DiceStatus.map((item) => {
+
+        let color = item.locked ? "green" : "white"
+
+        return(
+            <button className = "FinalProj--ParticularDice"
+                    key = {item.id}
+                    id = {item.id}
+                    onClick = {props.toggleDiceLock}
+                    style = {{backgroundColor : color}}
+                    >
+                        {item.number}
+            </button>
+        )
+    })
+
     return (
         <div className = "FinalProj--Dice">
-            <button className = "FinalProj--Button">1</button>
-            <button className = "FinalProj--Button">2</button>
-            <button className = "FinalProj--Button">3</button>
-            <button className = "FinalProj--Button">4</button>
-            <button className = "FinalProj--Button">5</button>
-            <button className = "FinalProj--Button">6</button>
-            <button className = "FinalProj--Button">7</button>
-            <button className = "FinalProj--Button">8</button>
-            <button className = "FinalProj--Button">9</button>
-            <button className = "FinalProj--Button">10</button>
+            {DiceElements}
         </div>
     );
 };
