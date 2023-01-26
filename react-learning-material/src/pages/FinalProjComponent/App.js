7898985a8093d9f73e2c437e72dc26ec0d2e84c3
AnsonCHOWcm <74531677+AnsonCHOWcm@users.noreply.github.com>
2023-01-26 20:27:59 +0800

    commit changes

--- react-learning-material/src/pages/FinalProjComponent/App.js
+++ react-learning-material/src/pages/FinalProjComponent/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import Dice from "./Dice";
 import Button from "./Button";
+import GameEnteringQ from "./GameEnteringQ";
 
 const App = () => {
+
+    const [numberDice, setnumberDice] = React.useState({
+        "number" : 0,
+        "messageShown" : "Please enter the number of Dices"
+    })
+
+    const mode = numberDice.number
+
     return (
         <div className = "FinalProj--App">
             <h1 className = "FinalProj--App--Title">Tenzies</h1>
@@ -10,6 +19,8 @@ const App = () => {
                 <br></br>
                 Click each dice to freeze it at its current value between rolls.
             </p>
+            {mode ? <Dice numberDice = {numberDice}/> : <GameEnteringQ numberDice = {numberDice}/>}
+            <Button numberDice = {numberDice}/>
         </div>
     );
 };
